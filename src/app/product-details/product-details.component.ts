20c62fa70fdb779e1e3b81a64b6372699dd6659d
wguzik <wojciech.guzik@gmail.com>
2022-08-21 10:15:26 +0200

    routing added for product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,7 +9,17 @@ import { Product, products } from '../products';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  constructor() {}
+  
+  product: Product | undefined;
 
-  ngOnInit(): void {}
+   constructor(private route: ActivatedRoute) { }
+
+   ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+  
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
